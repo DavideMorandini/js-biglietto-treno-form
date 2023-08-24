8d3a4318e738547d136cbcf365c0747bf65b7f7d
Davide <davidemorandini98@gmail.com>
2023-08-24 18:48:46 +0200

    biglietto treno form def

--- assets/JS/myscript.js
+++ assets/JS/myscript.js
@@ -22,49 +22,50 @@
 */
 
 // Pseudo-codice:
-// Chiedere all'utente il n. di km che desidera percorrere
-const userName = document.getElementById("Input_Name_Surname").value;
-const userSurname = document.getElementById("Input_Name_Surname").value;
-const userKm = document.getElementById("Input_Km").value;
-// const userAge = document.getElementById("Input_user_age").value;
-
+// Chiedere all'utente nome, cognome, l'età e il n. di km che desidera percorrere
 
 document.getElementById("generate").addEventListener("click", function() {
 
-    const resultUser = document.getElementById("Result_Name_and_Surname").value;
-    console.log(resultUser);
+    const userName = document.getElementById("Input_Name_Surname").value;
+    console.log(userName);
 
-    const Km = document.getElementById("Result_Km").value;
-    console.log(Km);
+    const userKm = document.getElementById("Input_Km").value;
+    console.log(userKm);
+
+    const userAge = document.getElementById("Input_user_age").value;
+    console.log(userAge);
+   
+    // Prezzo del biglietto al Km percorso
+    const ticketPriceFor1Km = 0.21;
+
+    // Calcolare il prezzo STANDARD (cioè SENZA sconti) del biglietto
+    const ticketStandardPrice = (userKm * ticketPriceFor1Km).toFixed(2);
+
+    //  Calcolare il prezzo dei biglietti in base agli SCONTI
+    const userUnder18 = (ticketStandardPrice / 100 * 20).toFixed(2);
+    const userOver65 = (ticketStandardPrice / 100 * 40).toFixed(2);
+
+    // Verifico le condizioni in base all'età degli utenti
+    if (userAge < 18) {
+        console.log("Complimenti! Hai diritto ad uno sconto del 20%" + " " + "Il prezzo del biglietto è di €" + " " + userUnder18);
+
+    } else if (userAge > 65) {
+        console.log("Complimenti! Hai diritto ad uno sconto del 40%" + " " + "Il prezzo del biglietto è di €" + " " + userOver65);
+
+    } else {
+        console.log("Siamo spiacenti! Non hai diritto a nessuno sconto" + " " + "Il prezzo del biglietto è di €" + " " + ticketStandardPrice);
+    }
+
+    document.getElementById("Result_Name_and_Surname").innerHTML = userName;
+
+    document.getElementById("Result_Km").innerHTML = userKm;
+
+    document.getElementById("Result_Age").innerHTML = userAge;    
 })
 
 
 
 
-// Prezzo del biglietto al Km percorso
-const ticketPriceFor1Km = 0.21;
-// console.log(ticketPriceFor1Km);
-
-// Calcolare il prezzo STANDARD (cioè SENZA sconti) del biglietto
-const ticketStandardPrice = (userKm * ticketPriceFor1Km).toFixed(2);
-// console.log(ticketStandardPrice);
-
-//  Calcolare il prezzo dei biglietti in base agli SCONTI
-const userUnder18 = (ticketStandardPrice / 100 * 20).toFixed(2);
-const userOver65 = (ticketStandardPrice / 100 * 40).toFixed(2);
-
-// console.log(userUnder18, userOver65);
-
-// Verifico le condizioni in base all'età degli utenti
-if (userAge < 18) {
-    console.log("Complimenti! Hai diritto ad uno sconto del 20%" + " " + "Il prezzo del biglietto è di €" + " " + userUnder18);
-
-} else if (userAge > 65) {
-    console.log("Complimenti! Hai diritto ad uno sconto del 40%" + " " + "Il prezzo del biglietto è di €" + " " + userOver65);
-
-} else {
-    console.log("Siamo spiacenti! Non hai diritto a nessuno sconto" + " " + "Il prezzo del biglietto è di €" + " " + ticketStandardPrice);
-}
 
 
 
